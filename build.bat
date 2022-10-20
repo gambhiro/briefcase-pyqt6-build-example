f195703c79574b35d5b7a1119acc8233c1a2234e
Gambhiro <gambhiro@users.noreply.github.com>
2022-10-20 20:51:05 +0200

    upd

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ pyinstaller run.py ^
     --name smallapp ^
     --onefile ^
     -w ^
-    --add-data smallapp/assets:smallapp/assets
+    --add-data "smallapp\assets;smallapp\assets"
 
 :: Ensure blank line after cmd with caret
 echo "OK"
